c33a97a4783ef08f871f888710d7a89213a6da6a
bijay-1tamang <79555398+bijay-1tamang@users.noreply.github.com>
2024-06-18 19:01:55 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,7 @@ numberInput.addEventListener('input',() => {
     const value = parseFloat(numberInput.value);
     if(value <= -10 || value >= 10)
         alert('The number should be between -10 and 10');
-})
-
-
+});
 
 const fruit = {
     Name: 'Apple',
@@ -18,7 +16,7 @@ console.log(fruit);
 
 
 // Printing our car information to the page
-document.querySelector('#Name').innerHTML = fruit.Name;
-document.querySelector('#Color').innerHTML = fruit.Color;
-document.querySelector('#Taste').innerHTML = fruit.Taste;
+document.querySelector('#fruitName').innerHTML = 'Name: ${fruit.Name}';
+document.querySelector('#fruitColor').innerHTML = 'Color: ${fruit.Color}';
+document.querySelector('#fruitTaste').innerHTML = 'Taste: ${fruit.Taste}';
 
